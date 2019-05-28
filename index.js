11e5d7483f72e759c5264fa6229210073ca02ca4
Andy Bettisworth <andy@accreu.com>
2019-05-28 14:03:15 -0500

    Add CORS middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 
 const express = require('express')
+const cors = require('cors')
 
 /**
  * Constants
@@ -17,6 +18,12 @@ const port = 8080
  */
 
 const app = express()
+
+/**
+ * Middleware
+ */
+
+app.use(cors())
 
 /**
  * Routes
