1c7ef734147a6e7817343d5dee692f5ec64b9e5b
Andy Bettisworth <andy@accreu.com>
2019-05-28 14:42:09 -0500

    Add express.urlencoded middleware and POST for new posts

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const app = express()
  */
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 /**
--- routes/posts_router.js
+++ routes/posts_router.js
@@ -28,6 +28,19 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  try {
+    console.log('req.body', req.body)
+    let results = await db.insert(req.body)
+    console.log('results', results)
+
+    res.sendStatus(200)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: { message: 'Server error' }})
+  }
+})
+
 router.get('/:id', async (req, res) => {
   try {
     let post = await db.findById(req.params.id)
@@ -43,6 +56,28 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  try {
+    await db.update(req.params.id, req.body)
+
+    res.sendStatus(200)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: { message: 'Server error' }})
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await db.remove(req.params.id)
+
+    res.sendStatus(200)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: { message: 'Server error' }})
+  }
+})
+
 router.get('/:id/comments', async (req, res) => {
   try {
     let comments = await db.findPostComments(req.params.id)
@@ -52,6 +87,17 @@ router.get('/:id/comments', async (req, res) => {
     } else {
       res.status(404).json({ error: { message: 'Post not found' }})
     }
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: { message: 'Server error' }})
+  }
+})
+
+router.post('/:id/comments', async (req, res) => {
+  try {
+    await db.insertComment(req.body)
+
+    res.sendStatus(200)
   } catch(err) {
     console.error(err)
     res.status(500).json({ error: { message: 'Server error' }})
