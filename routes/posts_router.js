7168919ba2619dd1799d18b00626a98c14d14029
Andy Bettisworth <andy@accreu.com>
2019-05-28 15:25:42 -0500

    Match desired API results

--- routes/posts_router.js
+++ routes/posts_router.js
@@ -24,20 +24,27 @@ router.get('/', async (req, res) => {
     res.status(200).json(posts)
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'The posts information could not be retrieved.' }})
   }
 })
 
 router.post('/', async (req, res) => {
   try {
-    console.log('req.body', req.body)
-    let results = await db.insert(req.body)
-    console.log('results', results)
+    if (!req.body.title || !req.body.contents) {
+      return res.status(400).json({ error: { message: 'Please provide title and contents for the posts.' }})
+    }
 
-    res.sendStatus(200)
+    let results = await db.insert(req.body)
+
+    if (results.id) {
+      let post = await db.findById(results.id)
+      res.status(201).json(post)
+    } else {
+      res.status(400).json({ error: { message: 'No post was created.' }})
+    }
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'There was an error while saving the post to the database.' }})
   }
 })
 
@@ -48,22 +55,33 @@ router.get('/:id', async (req, res) => {
     if (post) {
       res.status(200).json(post)
     } else {
-      res.status(404).json({ error: { message: 'Post not found' }})
+      res.status(404).json({ error: { message: 'The post with the specified ID does not exist.' }})
     }
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'The post information could not be retrieved.' }})
   }
 })
 
 router.put('/:id', async (req, res) => {
   try {
-    await db.update(req.params.id, req.body)
+    let posts = await db.findById(req.params.id)
 
-    res.sendStatus(200)
+    if (posts.length > 0) {
+      if (!req.body.title || !req.body.contents) {
+        res.status(400).json({ errors: { message: 'Please provide title and contents for the post.' }})
+      } else {
+        await db.update(req.params.id, req.body)
+
+        let posts = await db.findById(req.params.id)
+        res.status(200).json(posts[0])
+      }
+    } else {
+      res.status(404).json({ error: { message: 'The post with the specified ID does not exist.' }})
+    }
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'The post information could not be modified.' }})
   }
 })
 
@@ -80,27 +98,37 @@ router.delete('/:id', async (req, res) => {
 
 router.get('/:id/comments', async (req, res) => {
   try {
-    let comments = await db.findPostComments(req.params.id)
+    let posts = await db.findById(req.params.id)
 
-    if (comments) {
+    if (posts.length > 0) {
+      let comments = await db.findPostComments(req.params.id)
       res.status(200).json(comments)
     } else {
-      res.status(404).json({ error: { message: 'Post not found' }})
+      res.status(404).json({ error: { message: 'The post with the specified ID does not exist.' }})
     }
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'The comments information could not be retrieved.' }})
   }
 })
 
 router.post('/:id/comments', async (req, res) => {
   try {
-    await db.insertComment(req.body)
+    let posts = await db.findById(req.params.id)
 
-    res.sendStatus(200)
+    if (posts.length > 0) {
+      if (!req.body.text) {
+        await db.insertComment(req.body)
+        res.status(201).json({ error: { message: 'There was an error while saving the comment to the database.' }})
+      } else {
+        res.status(400).json({ error: { message: 'Please provide text for the comment.' }})
+      }
+    } else {
+      res.status(404).json({ error: { message: 'The post with the specified ID does not exist.' }})
+    }
   } catch(err) {
     console.error(err)
-    res.status(500).json({ error: { message: 'Server error' }})
+    res.status(500).json({ error: { message: 'There was an error while saving the comment to the database.' }})
   }
 })
 
